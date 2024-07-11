cf50c1d545389c72dfb7bd5fd2d0c0520c9d7fd2
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-11 10:29:25 +0300

    create table 'users' with ENUM constraint

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- script to create table 'users'
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US',
+	PRIMARY KEY (id)
+)
