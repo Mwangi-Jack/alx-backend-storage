9f15f226a4c1326563e21c06a71f3335cc97efe8
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-17 20:13:24 +0300

    storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,26 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    This function is a decorator function that takes in a single
+    'callable' function and returns a callable
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        output_key = f'{method.__qualname__}:output'
+        input_key = f'{method.__qualname__}:input'
+
+        output = method(self, str(args))
+
+        self._redis.rpush(input_key, str(args))
+        self._redis.rpush(output_key, output)
+
+        return output
+
+    return wrapper
+
+
 class Cache:
     """
     Cache class defines two methods the initializaiton method
@@ -38,6 +58,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         This method takes in a 'data' argument generates a random key
