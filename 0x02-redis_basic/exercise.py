3f027c1ba301b76528afdc355d64ec0d847432f1
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-17 17:54:36 +0300

    writing strings to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,15 +12,15 @@ class Cache:
     and the store method
     """
 
-    def __init__(self) -> None:
+    def __init__(self: "Cache") -> None:
         """
         this method initializes the class with a private
         variable '_redis' which holds the redis instance
         """
-        self._redis = redis.Redis()
+        self._redis: redis.Redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, bytes, int]) -> str:
+    def store(self: "Cache", data: Union[str, float, bytes, int]) -> str:
         """
         This method takes in a 'data' argument generates a random key
         ,stores the input data in Redis using the random keyand returns
