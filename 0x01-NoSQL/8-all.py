e71e01e71716cde9415d5de8f72a8e42e7c31a52
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-17 12:10:12 +0300

    python code to list documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""listing all documents in a collection"""
+
+def list_all(mongo_collection) :
+    """
+    This function takes in a pymongo collection object
+    'mongo_collection' as a parameter and returns a list of all
+    the dcuments in the collection or empty list if not documents found
+    """
+
+    if not mongo_collection:
+        return []
+
+    documents = mongo_collection.find()
+
+    return [entry for entry in documents]
